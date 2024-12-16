82448321a959e1f938da246411d471231a6f02fe
GUYEIEI <guy18042003@gmail.com>
2024-12-17 02:04:00 +0700

    feat: implement database transaction for deleting comments

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -2,6 +2,7 @@ import { userModel } from "../../model/user.Model";
 import { CommentModel } from "../../model/comment.Model";
 import {createCommentType, DeleteCommentType} from "../../types/comment.type";
 import { noteModel } from "../../model/note.Model";
+import mongoose from "mongoose";
 
 class commmentService{
     async create({content, noteId, author}: createCommentType):Promise<createCommentType>{
@@ -22,25 +23,39 @@ class commmentService{
             throw new Error('Id is required')
         }
 
-        const deleteComment =  await CommentModel.findOne({
-            _id: id,
-            author: accountOwnerId,
-        }); 
-    
-        if (!deleteComment) {
-            throw new Error("Cannot be deleted");
-        }
-        
-        const deleteCommentInNote = await noteModel.findByIdAndDelete(id);
+        const session = await mongoose.startSession();
 
-        await noteModel.findByIdAndUpdate(deleteComment.noteId, {
-            $pull: { comments: id },
-        });
-        await userModel.findByIdAndUpdate(accountOwnerId, {
-            $pull: { comments: id },
-        });
-    
-        return deleteCommentInNote;
+        try {
+            session.startTransaction();
+
+            const deleteComment = await CommentModel.findOne({
+                _id: id,
+                author: accountOwnerId,
+            }).session(session); 
+
+            if (!deleteComment) {
+                throw new Error("Cannot be deleted");
+            }
+
+            await CommentModel.findByIdAndDelete(id).session(session);
+
+            await noteModel.findByIdAndUpdate(deleteComment.noteId, {
+                $pull: { comments: id },
+            }).session(session);   
+
+            await userModel.findByIdAndUpdate(accountOwnerId, {
+                $pull: { comments: id },
+            }).session(session); 
+
+            await session.commitTransaction();
+            session.endSession();
+
+            return deleteComment;
+        } catch (error) {
+            await session.abortTransaction();
+            session.endSession();
+            throw error;
+        }
     }
 }
 
