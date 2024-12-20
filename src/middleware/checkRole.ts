09ec8ad10f6d037b7b103f4aaade461d0e793760
GUYEIEI <guy18042003@gmail.com>
2024-12-20 23:40:30 +0700

    fix:  Improve checkRole middleware logic for better role validation refactor: Extract authenticateToken middleware into separate file

--- src/middleware/checkRole.ts
+++ src/middleware/checkRole.ts
@@ -1,20 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 
 // Middleware ตรวจสอบ Role
-export const checkRole = (allowedRoles: string[]) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const userRole = req.headers["role"]; // รับ role จาก headers หรือ token ตามที่กำหนดไว้
+export const checkRole = (allowedRoles: [string]) => (req: Request, res: Response, next: NextFunction) => {
+  const userRole = (req as any).user.role;
 
-    // ตรวจสอบว่า userRole มีค่าหรือไม่
-    if (!userRole) {
-      return res.status(401).json({ message: "Unauthorized: No role provided" });
-    }
+  if (!userRole) {
+    return res.status(401).json({ message: "Unauthorized: No role provided" });
+  }
 
-    // ตรวจสอบว่า userRole อยู่ใน allowedRoles หรือไม่
-    if (!allowedRoles.includes(userRole.toString())) {
-      return res.status(403).json({ message: "Forbidden: Access denied" });
-    }
+  if (!allowedRoles.includes(userRole)) {
+    return res.status(403).json({ message: "Forbidden: Access denied" });
+  }
 
-    next(); // ให้ผ่านไปยัง Controller ต่อไป
-  };
+  next(); 
 };
