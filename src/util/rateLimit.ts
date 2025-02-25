9c336b72e1785c0563a63dcf4192109073e2bc93
GUYEIEI <guy18042003@gmail.com>
2025-02-26 00:30:56 +0700

    edit rate limit

--- src/util/rateLimit.ts
+++ src/util/rateLimit.ts
@@ -2,14 +2,14 @@ import rateLimit from 'express-rate-limit'
 
 export const authRateLimiter = rateLimit({
     windowMs: 5 * 60 * 1000, 
-    max: 100,
+    max: 50,
     message: "Too many login attempts. Please try again later.",
 });
   
 
 export const publicRateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, 
-    max: 200,
+    max: 300,
     message: "You have exceeded the rate limit.",
 });
 
